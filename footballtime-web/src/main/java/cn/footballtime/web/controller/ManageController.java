b3046d708a8bbd6ba07855775c2a8eb3fedaf4f9
LiGuochao <liguochao1020@163.com>
2017-01-05 09:19:43 +0800

    aa

---
+++ footballtime-web/src/main/java/cn/footballtime/web/controller/ManageController.java
@@ -0,0 +1,47 @@
+package cn.footballtime.web.controller;
+
+import cn.footballtime.utils.security.AESUtil;
+import cn.footballtime.web.config.AppSetting;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+/**
+ * Created by Administrator on 2017/1/4.
+ */
+@Controller
+@RequestMapping("/manage")
+public class ManageController {
+    @RequestMapping(value = "/login")
+    public String login() {
+        return "manage/login";
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public void login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        String userName=request.getParameter("userName");
+        String password=request.getParameter("password");
+
+
+
+        //使用request对象的getSession()获取session，如果session不存在则创建一个
+        HttpSession session = request.getSession();
+        //将数据存储到session中
+        session.setAttribute("userName", userName);
+
+        response.sendRedirect("index");
+    }
+
+    @RequestMapping("/index")
+    public void index(HttpServletResponse response)
+    {
+
+    }
+}
