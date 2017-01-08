6dd14414dfcc8303bde00848d767c43478d46968
LiGuochao <liguochao1020@163.com>
2017-01-08 22:54:12 +0800

    aa

--- footballtime-web/src/main/java/cn/footballtime/web/controller/ManageController.java
+++ footballtime-web/src/main/java/cn/footballtime/web/controller/ManageController.java
@@ -13,6 +13,9 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -72,6 +75,39 @@ public class ManageController {
         }
 
         model.addAttribute("userName", userName);
+
+        return "manage/index";
+    }
+
+    @RequestMapping(value = "/uploadpic",method = RequestMethod.POST)
+    public String uploadPic(HttpServletRequest request, HttpServletResponse response,Model model) throws Exception
+    {
+        String userName="";
+        HttpSession session = request.getSession();
+        if(session.getAttribute("uid")==null)
+        {
+            response.sendRedirect("login");
+        }
+        else
+        {
+            userName=session.getAttribute("uid").toString();
+        }
+
+        Date currentTime=new Date();
+        SimpleDateFormat formatDate = new   SimpleDateFormat("yyyyMM");
+        String folder = formatDate.format(currentTime);
+        File uploadPath = new File(request.getServletContext().getRealPath("/")+"upload/images/"+folder+"/");
+//应保证在根目录中有此目录的存在  如果没有，下面则先创建新的文件夹
+        if(!uploadPath.exists())
+        {
+            //System.out.println("创建新文件夹成功"+formatnowtime);
+            uploadPath.mkdir();
+        }
+        int returnflag = 0;
+        //SmartUpload mySmartUpload =new SmartUpload();
+        int file_size_max=1024000;
+        String ext="";
+        String url="upload/images/"+folder+"/";
 
         return "manage/index";
     }
